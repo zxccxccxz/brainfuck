95bb4bcaf2543af2cab5792cee20802965965c1a
toor <toor@toors-MBP.lan>
2024-01-31 15:42:57 +0200

    Refactored all code

--- src/lib.rs
+++ src/lib.rs
@@ -1,161 +1,167 @@
 use std::collections::HashMap;
-use std::fs;
+use std::{fmt, fs, error};
 
-#[derive(Clone)]
-enum BfToken {
-    IncrDataPtr,
-    DecrDataPtr,
-    IncrCurrent,
-    DecrCurrent,
+#[derive(Debug)]
+pub struct BuildError {
+    msg: String,
+}
+
+#[derive(Debug)]
+pub struct RunError {
+    msg: String,
+}
+
+impl BuildError {
+    fn new(msg: String) -> BuildError {
+        BuildError { msg: msg }
+    }
+}
+
+impl RunError {
+    fn new(msg: String) -> RunError {
+        RunError { msg: msg }
+    }
+}
+
+impl error::Error for BuildError {}
+impl error::Error for RunError {}
+
+
+impl fmt::Display for BuildError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Build error: {}", self.msg)
+    }
+}
+
+impl fmt::Display for RunError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Run error: {}", self.msg)
+    }
+}
+
+pub type Program = Vec<BfToken>;
+
+pub struct BFI;
+
+#[derive(Clone, Debug)]
+pub enum BfToken {
+    IncermentDataPtr,
+    DecrementDataPtr,
+    IncrementCurrent,
+    DecrementCurrent,
     OutputByte,
     InputByte,
     JumpStart(usize),
     JumpEnd(usize),
-    Unrecognized,
-}
-
-pub struct BFI {
-    program: Vec<BfToken>,
-    memory: [i32; 30000],
-    inst_ptr: usize,
-    data_ptr: usize,
-    jump_locations: HashMap<usize, usize>, // end -> start
-}
-
-impl BfToken {
-    fn is_valid(&self) -> bool {
-        !matches!(*self, BfToken::Unrecognized)
-    }
 }
 
 impl BFI {
-    pub fn new() -> BFI {
-        BFI {
-            program: Vec::new(),
-            memory: [0; 30000],
-            inst_ptr: 0,
-            data_ptr: 0,
-            jump_locations: HashMap::new(),
+    pub fn build(code: String) -> Result<Program, BuildError> {
+        println!("Building...");
+        // Tokenize
+        let bf_commands = "<>+-.,[]";
+        let code: Vec<char> = code
+        .chars()
+        .filter(|&c| bf_commands.contains(c))
+        .collect();
+        //println!("Code: {:?}", &code);
+        let mut program: Program = Vec::new();
+        let mut jump_locations: HashMap<usize, usize> = HashMap::new();
+        for (i, c) in code.iter().enumerate() {
+            let token = match c {
+                '>' => BfToken::IncermentDataPtr,
+                '<' => BfToken::DecrementDataPtr,
+                '+' => BfToken::IncrementCurrent,
+                '-' => BfToken::DecrementCurrent,
+                '.' => BfToken::OutputByte,
+                ',' => BfToken::InputByte,
+                '[' => {
+                    let jump_end_location = Self::locate_jump_end(i, &code[i+1..])?;
+                    jump_locations.insert(jump_end_location, i);
+                    BfToken::JumpStart(jump_end_location+1)
+                },
+                ']' => {
+                    if let Some(&jump_start_location) = jump_locations.get(&i) {
+                        BfToken::JumpEnd(jump_start_location+1)
+                    } else {
+                        return Err(BuildError::new(format!("Failed to find jump start for {i}")));
+                    }
+                },
+                _ => { return Err(BuildError::new(format!("Unexpected token at {i}"))); },
+            };
+            program.push(token);
         }
+        println!("{:?}", jump_locations);
+        println!("Successfully built");
+        Ok(program)
     }
 
-    fn read_file_from_args(mut args: impl Iterator<Item = String>) -> Result<String, String> {
-        args.next();
-        let filename = match args.next() {
-            Some(filename) => filename,
-            None => {
-                return Err(format!("Not enough arguments, please provide file which contains brainfuck code."));
+    pub fn run(program: Program) -> Result<(), RunError> {
+        println!("Running...");
+        let mut memory: [i32; 30000] = [0; 30000];
+        let mut instruction_ptr: usize = 0;
+        let mut data_ptr: usize = 0;
+
+        loop {
+            if instruction_ptr >= program.len() {
+                break;
             }
-        };
-        fs::read_to_string(filename).or_else(|err| Err(format!("Failed to locate file: {err}")))
+            match program[instruction_ptr] {
+                BfToken::IncermentDataPtr => data_ptr += 1,
+                BfToken::DecrementDataPtr => data_ptr -= 1,
+                BfToken::IncrementCurrent => memory[data_ptr] += 1,
+                BfToken::DecrementCurrent => memory[data_ptr] -= 1,
+                BfToken::OutputByte => print!("{}", memory[data_ptr] as u8 as char),
+                BfToken::InputByte => {
+                    let mut buff = String::new();
+                    if let Err(err) = std::io::stdin().read_line(&mut buff) {
+                        return Err(RunError::new(format!("User input failure at {} with {}", instruction_ptr, err)));
+                    }
+                    memory[data_ptr] = buff.trim().parse::<i32>().unwrap();
+                },
+                BfToken::JumpStart(jump_to) => {
+                    if memory[data_ptr] == 0 {
+                        instruction_ptr = jump_to;
+                        continue;
+                    }
+                },
+                BfToken::JumpEnd(jump_to) => {
+                    if memory[data_ptr] != 0 {
+                        instruction_ptr = jump_to;
+                        continue;
+                    }
+                },
+            }
+            instruction_ptr += 1;
+        }
+        println!();
+        Ok(())
     }
 
-    fn locate_jump_end(jump_start_location: usize, program: &[BfToken]) -> Result<usize, String> {
+    fn locate_jump_end(jump_start_location: usize, program: &[char]) -> Result<usize, BuildError> {
         let mut to_skip: usize = 0;
         let mut curr_inst: usize = jump_start_location + 1;
         for c in program {
             match c {
-                BfToken::JumpStart(_) => {
+                '[' => {
                     to_skip += 1;
                 }
-                BfToken::JumpEnd(_) if to_skip > 0 => {
+                ']' if to_skip > 0 => {
                     to_skip -= 1;
                 },
-                BfToken::JumpEnd(_) => {
+                ']' => {
                     return Ok(curr_inst);
                 }
                 _ => (),
             };
             curr_inst += 1;
         }
-        Err(format!("Failed to find end of jump at instruction {jump_start_location}"))
+        Err(BuildError::new(format!("Failed to find end of jump at instruction {jump_start_location}")))
     }
+}
 
-    pub fn build(&mut self, args: impl Iterator<Item = String>) -> Result<(), String> {
-        println!("Building...");
-        let code = Self::read_file_from_args(args)?;
-        // Tokenize
-        self.program.clear();
-        self.jump_locations.clear();
-        let mut program = Vec::new();
-        for c in code.bytes() {
-            let token = match c as char {
-                '>' => BfToken::IncrDataPtr,
-                '<' => BfToken::DecrDataPtr,
-                '+' => BfToken::IncrCurrent,
-                '-' => BfToken::DecrCurrent,
-                '.' => BfToken::OutputByte,
-                ',' => BfToken::InputByte,
-                '[' => BfToken::JumpStart(0),
-                ']' => BfToken::JumpEnd(0),
-                _ => BfToken::Unrecognized,
-            };
-            if token.is_valid() {
-                program.push(token);
-            }
-        }
-        // Handle jumps
-        let mut curr_inst: usize = 0;
-        self.program = program.clone();
-        for inst in &mut self.program {
-            match inst {
-                BfToken::JumpStart(jump_to) => {
-                    let end_location = Self::locate_jump_end(curr_inst, &program[curr_inst+1..])?;
-                    self.jump_locations.insert(end_location, curr_inst);
-                    *jump_to = end_location + 1;
-                },
-                BfToken::JumpEnd(jump_to) => {
-                    if let Some(start_location) = self.jump_locations.get(&curr_inst) {
-                        *jump_to = start_location + 1;
-                    } else {
-                        return Err(format!("Failed to find jump start at instruction {curr_inst}"));
-                    }
-                },
-                _ => (),
-            }
-            curr_inst += 1;
-        }
-        println!("Successfully built");
-        Ok(())
-    }
-
-    pub fn run(&mut self) -> Result<(), String> {
-        loop {
-            if self.inst_ptr >= self.program.len() {
-                break;
-            }
-            match self.program[self.inst_ptr] {
-                BfToken::IncrDataPtr => self.data_ptr += 1,
-                BfToken::DecrDataPtr => self.data_ptr -= 1,
-                BfToken::IncrCurrent => self.memory[self.data_ptr] += 1,
-                BfToken::DecrCurrent => self.memory[self.data_ptr] -= 1,
-                BfToken::OutputByte => print!("{}", self.memory[self.data_ptr] as u8 as char),
-                BfToken::InputByte => {
-                    let mut buff = String::new();
-                    if let Err(err) = std::io::stdin().read_line(&mut buff) {
-                        return Err(format!("User input failure at {} with {}", self.inst_ptr, err));
-                    }
-                    self.memory[self.data_ptr] = buff.trim().parse::<i32>().unwrap();
-                },
-                BfToken::JumpStart(jump_to) => {
-                    if self.memory[self.data_ptr] == 0 {
-                        self.inst_ptr = jump_to;
-                        continue;
-                    }
-                },
-                BfToken::JumpEnd(jump_to) => {
-                    if self.memory[self.data_ptr] != 0 {
-                        self.inst_ptr = jump_to;
-                        continue;
-                    }
-                },
-                BfToken::Unrecognized => {
-                    return Err(format!("Unexpected token at instruction {}", self.inst_ptr));
-                }
-            }
-            self.inst_ptr += 1;
-        }
-        println!();
-        Ok(())
-    }
+pub fn read_file_from_args(mut args: impl Iterator<Item = String>) -> Result<String, Box<dyn error::Error>> {
+    args.next();
+    let filename = args.next().ok_or_else(|| "Filepath is not supplied in args")?;
+    Ok(fs::read_to_string(filename)?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-use std::{process, env};
 use std::time::Instant;
 use brainfuck::BFI;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start_time = Instant::now();
 
-    let mut bfi = BFI::new();
-    if let Err(err) = bfi.build(env::args()) {
-        eprintln!("Build error: {err}");
-        process::exit(1);
-    }
-    if let Err(err) = bfi.run() {
-        eprintln!("Error while executing program: {err}");
-        process::exit(1);
-    }
+    let code = brainfuck::read_file_from_args(std::env::args())?;
+    let program= BFI::build(code)?;
+    BFI::run(program)?;
 
     let elapsed_time = start_time.elapsed();
     println!("Elapsed time: {:?}", elapsed_time);
+    Ok(())
 }
